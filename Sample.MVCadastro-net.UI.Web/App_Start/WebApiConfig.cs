dcde9f97da978d9ac27b793e86f768c376c7e316
rodrigolsr <rodrigolsr@gmail.com>
2013-07-22 18:23:49 -0300

    Aplicando pacotes nuget Unity, MVC, WebGrease

---
+++ Sample.MVCadastro-net.UI.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample.MVCadastro_net.UI.Web
+{
+    public class WebApiConfig
+    {
+    }
+}

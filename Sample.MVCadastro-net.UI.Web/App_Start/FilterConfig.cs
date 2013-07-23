4566393edcd22787c527d4a7db7b7edd9ef090c0
rodrigolsr <rodrigolsr@gmail.com>
2013-07-23 20:00:57 -0300

    Adicionando App_Start

--- Sample.MVCadastro-net.UI.Web/App_Start/FilterConfig.cs
+++ Sample.MVCadastro-net.UI.Web/App_Start/FilterConfig.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Sample.MVCadastro_net.UI.Web
 {
-    public class FilterConfig
+    public static class FilterConfig
     {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
     }
 }

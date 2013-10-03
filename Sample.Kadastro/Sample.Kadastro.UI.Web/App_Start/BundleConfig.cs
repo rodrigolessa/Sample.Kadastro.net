2db0634ea9df3779f1ae856c73a0dd8f7ab0a1ed
rodrigolsr <rodrigolsr@gmail.com>
2013-10-03 17:53:35 -0300

    APlicando pacotes com nuget

---
+++ Sample.Kadastro/Sample.Kadastro.UI.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample.Kadastro.UI.Web.App_Start
+{
+    public class BundleConfig
+    {
+    }
+}
--- Sample.Kadastro/Sample.Kadastro.UI.Web/Global.asax.cs
+++ Sample.Kadastro/Sample.Kadastro.UI.Web/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace Sample.Kadastro.UI.Web
 {
@@ -12,29 +13,14 @@ namespace Sample.Kadastro.UI.Web
 
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-        }
-
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
 
-            RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Bootstrapper.Initialise();
         }
     }
 }
